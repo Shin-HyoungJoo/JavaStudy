f6403f6cb6b822934f2df2d1eb119e94fb3fa2a9
신형주 <gbz3267@naver.com>
2024-07-30 22:41:54 +0900

    자바공부 - 인프런 강의 공부

---
+++ src/chapter1/ClassStart3.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class ClassStart3 {
+    public static void main(String[] args) {
+        Student student1 = new Student("학생1",15,90);
+        Student student2 = new Student("학생2",16,80);
+
+        student1.getStudentInfo();
+        student2.getStudentInfo();
+
+        //참조값 출력
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
