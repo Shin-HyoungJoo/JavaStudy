4ef90f0dbbdb48194a93557b29df8661156e3fa8
gbz3267 <gbz3267@gmail.com>
2024-08-06 17:31:51 +0900

    다형성, 추상클래스, 인터페이스

---
+++ src/poly/ex6/Bird.java
@@ -0,0 +1,16 @@
+package poly.ex6;
+
+public class Bird extends AbstractAnimal implements Fly{
+
+	@Override
+	public void fly() {
+		System.out.println("새날기");
+	}
+
+	@Override
+	public void sound() {
+		System.out.println("짹짹");
+	}
+	
+
+}
