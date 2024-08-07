4b2353dfe995dd528d5d8ea9cf82045278d4894d
gbz3267 <gbz3267@gmail.com>
2024-08-07 15:29:35 +0900

    String

---
+++ src/mid/one/lang/string/equals/StringImmutable1.java
@@ -0,0 +1,9 @@
+package mid.one.lang.string.equals;
+
+public class StringImmutable1 {
+	public static void main(String[] args) {
+		String str = "hello";
+		str.concat(" java");
+		System.out.println("str = " + str);
+	}
+}
