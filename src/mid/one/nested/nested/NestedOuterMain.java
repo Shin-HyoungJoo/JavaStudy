077c7b0c04633580fd0ceda35c66cc9f0cc3d08f
gbz3267 <gbz3267@gmail.com>
2024-08-12 17:15:59 +0900

    중첩, 내부클래스1

---
+++ src/mid/one/nested/nested/NestedOuterMain.java
@@ -0,0 +1,9 @@
+package mid.one.nested.nested;
+
+public class NestedOuterMain {
+	public static void main(String[] args) {
+		NestedOuter outer = new NestedOuter();
+		NestedOuter.Nested nested = new NestedOuter.Nested();
+		nested.print();
+	}
+}
